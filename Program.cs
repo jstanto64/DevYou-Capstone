31f9ea2e25e81146ed193bdd725d8c6a2116b896
jstanto64 <73408019+jstanto64@users.noreply.github.com>
2020-10-30 15:51:46 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacationBudgetPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "J";
+
+
+
+            Console.WriteLine("Welcome to Vacation Budget Planner");
+
+            Console.WriteLine("Please enter your name");
+
+            Console.WriteLine("Welcome " + name + ", Thank you and where " +
+                "would you like to travel today? Mexico or Jamaica");
+
+
+            Console.ReadLine();
+        }
+    }
+}
